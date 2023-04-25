a287dbc0c6d8c559698bc75b759b0d05b06a1945
김태히 <k.tehi@gimtaehiui-MacBookPro.local>
2023-04-25 09:18:38 -0700

    components for Appointment section

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
-import Confirm from "components/Appointment/Confirm";
-import Status from "components/Appointment/Status";
+import Show from "components/Appointment/Show";
+// import Error from "components/Appointment/Error";
+// import Confirm from "components/Appointment/Confirm";
+// import Status from "components/Appointment/Status";
+// import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
   return (
-    <section className="appointment">
-      <Header />
-      <Empty />
+    <section
+      className="appointment"
+    >
+      <Header time={props.time} />
+      {props.interview && <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer.name}
+      />}
+      {!props.interview && <Empty />}
+      {/* <Error />
       <Confirm />
       <Status />
+      <Form /> */}
     </section>
   );
 }

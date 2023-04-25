0eafa6f4cec9cb75dcb5dd6c2c025e442cd23e6a
김태히 <k.tehi@gimtaehiui-MacBookPro.local>
2023-04-25 09:22:23 -0700

    import days API from scheduler-api and implement api data into <DayList>

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,70 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "components/Application.scss";
+import DayList from "components/DayList.js";
+import Appointment from "components/Appointment";
+import axios from "axios";
+// npm install axios@0.20.0
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
+
+  const [stateDay, setDay] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days').then(response => {
+      console.log(response.data);
+      setDay(response.data);
+    }).catch((error) => error.message)
+    
+  }, [setDay]);
+
+  const schedule = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +74,12 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={stateDay}
+            setDay={setDay}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -20,8 +87,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
       </section>
     </main>
   );
 }
+

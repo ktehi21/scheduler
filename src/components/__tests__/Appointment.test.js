ffc4407be97a95dd7c19378fe65767ec88c6bced
김태히 <k.tehi@gimtaehiui-MacBookPro.local>
2023-04-29 20:47:59 -0700

    test file

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Appointment from "components/Appointment/index.js";
+
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  xit("does something it is supposed to do", () => {
+
+  });
+
+  test.skip("does something else it is supposed to do", () => {
+    // ...
+  });
+
+  // Call Mock Functions
+  it("doesn't call the function", () => {
+    const fn = jest.fn();
+    expect(fn).toHaveBeenCalledTimes(0);
+  });
+
+  it("calls the function", () => {
+    const fn = jest.fn();
+    fn();
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls the function with specific arguments", () => {
+    const fn = jest.fn();
+    fn(10);
+    expect(fn).toHaveBeenCalledWith(10);
+  });
+
+  it("uses the mock implementation", () => {
+    const fn = jest.fn((a, b) => 42);
+    fn(1, 2);
+    expect(fn).toHaveReturnedWith(42);
+  });
+
+});
+
